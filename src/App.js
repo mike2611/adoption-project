1b6747eb826e86c19b68e83ddc137a6f45b66a98
mike2611 <mpuentes2611@gmail.com>
2022-03-09 10:48:48 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import ReactDOM from 'react-dom';
+import SearchParams from './SearchParams';
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanesse",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, { name: "Suda", animal: "Dog", breed: "Bulldog" }),
+//   ]);
+// };
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
